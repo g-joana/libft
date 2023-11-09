b95046eea071499fd8bb3cce5dbfe10112469544
jgils <gilsjoana@gmail.com>
2023-11-09 19:28:52 -0300

    add bonus functions

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgils <jgils@student.42.rio>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 19:04:47 by jgils             #+#    #+#             */
+/*   Updated: 2023/11/09 19:16:25 by jgils            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	t_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*newnext;
+
+	while (*lst)
+	{
+		newnext = lst->next;
+		del(lst->content);
+		free(lst);
+		lst = newnext;
+	}
+	*lst = NULL;
+}
