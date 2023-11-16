804a35007f55708a20cafa9d8aa4e2dd14fe0472
Joana <145489056+shift-j@users.noreply.github.com>
2023-11-16 10:42:33 -0300

    add ft_lstmap_bonus

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,24 +6,30 @@
 /*   By: jgils <jgils@student.42.rio>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:07:00 by jgils             #+#    #+#             */
-/*   Updated: 2023/11/13 12:52:30 by jgils            ###   ########.fr       */
+/*   Updated: 2023/11/16 10:29:12 by jgils            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap_bonus(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlst;
+	t_list	*newnode;
 
+	if (!lst || !f)
+		return (NULL);
+	newlst = NULL;
 	while (lst)
 	{
-		newlst->next = ft_lstnew_bonus(newlst);
-		if (!newlst->next)
-			ft_lstclear_bonus(&newlst, del);
-		newlst->next = f(lst->content);
+		newnode = ft_lstnew(f(lst->content));
+		if (!newnode)
+		{
+			ft_lstclear(&newlst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlst, newnode);
 		lst = lst->next;
 	}
-	newlst = NULL;
 	return (newlst);
 }
